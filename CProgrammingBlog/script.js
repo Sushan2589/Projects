44e3939f761b9dc6df64989bbd6333eb0a6fc759
Sushan <85663813+Sushan2589@users.noreply.github.com>
2025-03-12 20:51:07 +0545

    Update script.js

--- CProgrammingBlog/script.js
+++ CProgrammingBlog/script.js
@@ -3,3 +3,18 @@ function scrollToSection(sectionId) {
     section.scrollIntoView({ behavior: 'smooth' });
 }
 
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const menuButton = document.createElement("button");
+    menuButton.classList.add("menu-button");
+    menuButton.innerText = "☰";
+    document.body.appendChild(menuButton);
+
+    const leftMenu = document.querySelector(".left");
+
+    menuButton.addEventListener("click", function() {
+    leftMenu.classList.toggle("active");
+        });
+    });
+
